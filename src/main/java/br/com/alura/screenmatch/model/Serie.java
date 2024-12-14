f80accd63ba14cdb2ebbea553daba192917b7c90
Julio Cesar Perroni <julio_veia@hotmail.com>
2024-12-14 15:59:09 -0300

    Added categories

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,84 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.OptionalDouble;
+
+public class Serie {
+
+    private String title;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+
+    public Serie(DadosSerie dadosSerie) {
+        this.title = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.parseDouble(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+}
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,16 +1,13 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -20,37 +17,53 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     public void exibeMenu() {
         var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
-                
+                3 - Listar Séries
                 0 - Sair                                 
                 """;
+        var opcao = -1;
+        while (opcao != 0) {
+            System.out.println(menu);
+            try {
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
-
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+                switch (opcao) {
+                    case 1 -> buscarSerieWeb();
+                    case 2 -> buscarEpisodioPorSerie();
+                    case 3 -> listarSeries();
+                    case 0 -> System.out.println("Saindo...");
+                    default -> System.out.println("Opção inválida");
+                }
+            } catch (InputMismatchException | IllegalStateException e) {
+                System.out.println("Erro verificado na entrada do menu. Tente novamente.");
+                leitura.nextLine();
+            }
         }
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
-        System.out.println(dados);
+        dadosSeries.add(dados);
+        System.out.println(dados.titulo() + " incluída.");
+    }
+
+    private void listarSeries(){
+        dadosSeries.forEach(x -> {
+                    System.out.println("Nome da série: " + x.titulo());
+                    System.out.println("Nota: " + x.avaliacao());
+                    System.out.println("Total de temporadas: " + x.totalTemporadas());
+                    System.out.println("Genero: " + x.genero());
+                    System.out.println("Atores: " + x.atores());
+                    System.out.println("Pôster: " + x.poster());
+                    System.out.println("Sinopse: " + x.sinopse());
+                    System.out.println();
+                });
     }
 
     private DadosSerie getDadosSerie() {
@@ -72,4 +85,14 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
     }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
 }
